14d9b47917d7a986050f8ce4d072e5387766e009
shristi g <32318101+1shristi@users.noreply.github.com>
2025-05-08 10:45:00 -0600

    may this be blessed

--- scripts/imagenav2.js
+++ scripts/imagenav2.js
@@ -1,3 +1,21 @@
+const toggleDashboard = document.querySelectorAll('.toggle-dash');
+
+toggleDashboard.forEach(button => {
+    const targetId = button.getAttribute('data-target');
+    const img = document.getElementById(targetId);
+
+    button.addEventListener('click', function () {
+        if (img.style.display === 'none' || img.style.display === '') {
+            img.style.display = 'block';
+            button.textContent = 'Hide';
+        } else {
+            img.style.display = 'none';
+            button.textContent = 'Reveal';
+        }
+    });
+});
+
+
 ///
 // Get references to the buttons and the overlay image
 const showOverlay = document.getElementById('showMenu');
