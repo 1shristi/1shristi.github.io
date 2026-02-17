b5a0ab73cbccd46cbfc8a00c134ade8a2a4671eb
Shristi <32318101+1shristi@users.noreply.github.com>
2026-02-17 18:40:26 -0500

    moved unused content

--- scripts/hide-content.js
+++
@@ -1,7 +0,0 @@
-const hiddenContent = document.querySelector('.hidden-content');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    hiddenContent.classList.add('show-content');
-  }
-});
